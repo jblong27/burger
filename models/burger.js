8d6154daed4de1096ea389e526bed9000299c9ff
Jordan Long <jblong27@gmail.com>
2020-08-10 12:47:15 -0500

    pushing finished burger.js

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+var orm = require("../config/orm.js");
+
+var cat = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res)
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColvals, condition, function(res) {
+            cb(res);
+        });
+    },
+    deleteOne: function(condition, cb) {
+        orm.deleteOne("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = cat;
