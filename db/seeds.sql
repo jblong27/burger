91d6e11fe8bc09e0643a4f466c6a83f8c552d8d2
Jordan Long <jblong27@gmail.com>
2020-08-10 11:50:30 -0500

    connection.js finished

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Slug Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("borger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Nash Vegas", true);
